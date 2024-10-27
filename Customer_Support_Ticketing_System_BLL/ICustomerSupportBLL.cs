d7f4b6cf2ec14af3e4868485c259162175824163
warmlund <emeliewarmlund@gmail.com>
2024-10-27 14:09:51 +0100

    Set up GUI for main window

--- Customer_Support_Ticketing_System_BLL/ICustomerSupportBLL.cs
+++ Customer_Support_Ticketing_System_BLL/ICustomerSupportBLL.cs
@@ -8,5 +8,15 @@ namespace Customer_Support_Ticketing_System_BLL
 {
     internal interface ICustomerSupportBLL
     {
+        void LoadTicketsFromData();
+        void LoadCustomersFromData();
+        void SaveTicketsToData();
+        void SaveCustomersToData();
+        void AddNewTicket();
+        void AddNewCustomer();
+        void RemoveTicket(int ticketId);
+        void EditTicket(int ticketId);
+        void EditCustomer(int customerId);
+        void RemoveCustomer(int customerId);
     }
 }
