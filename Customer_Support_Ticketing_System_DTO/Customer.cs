24eaf3bd783a16576ccee111a80c032e99915615
warmlund <emeliewarmlund@gmail.com>
2024-10-29 08:55:55 +0100

    Commenting DTO layer

--- Customer_Support_Ticketing_System_DTO/Customer.cs
+++ Customer_Support_Ticketing_System_DTO/Customer.cs
@@ -2,18 +2,22 @@
 
 namespace Customer_Support_Ticketing_System_DTO
 {
+    /// <summary>
+    /// Class for Customer
+    /// Properties set for serialization and deserialization for JSON
+    /// </summary>
     public class Customer
     {
         [JsonProperty("CustomerId")]
-        public int CustomerId { get; set; }  // Unique identifier for the customer
+        public int CustomerId { get; set; }
 
         [JsonProperty("Customer")]
-        public string Name { get; set; } // Customer's first name
+        public string Name { get; set; }
 
         public Customer()
         {
             CustomerId = 0;
-            Name=String.Empty;
+            Name = string.Empty;
         }
     }
 }
--- Customer_Support_Ticketing_System_DTO/Ticket.cs
+++ Customer_Support_Ticketing_System_DTO/Ticket.cs
@@ -2,25 +2,29 @@
 
 namespace Customer_Support_Ticketing_System_DTO
 {
+    /// <summary>
+    /// Class for ticket
+    /// Properties set for serialization and deserialization for JSON
+    /// </summary>
     public class Ticket
     {
         [JsonProperty("TicketID")]
         public int TicketId { get; set; }
 
         [JsonProperty("Customer")]
-        public Customer Customer { get; set; }        
+        public Customer Customer { get; set; }
 
         [JsonProperty("Title")]
-        public string Title { get; set; }             
+        public string Title { get; set; }
 
         [JsonProperty("Description")]
-        public string Description { get; set; }      
+        public string Description { get; set; }
 
         [JsonProperty("Date")]
-        public DateTime CreatedDate { get; set; }     
+        public DateTime CreatedDate { get; set; }
 
         [JsonProperty("Status")]
-        public TicketStatus Status { get; set; }    
+        public TicketStatus Status { get; set; }
 
         [JsonProperty("Priority")]
         public TicketPriority Priority { get; set; }
